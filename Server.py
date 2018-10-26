a57703b19a28159165d753774905763471706d1d
LordWader <40608791+LordWader@users.noreply.github.com>
2018-10-26 10:18:54 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -5,7 +5,7 @@ class Root:
     @cherrypy.expose 
     def index(self):
         graph()
-        return "<img src='/images/foo.png'>"
+        return "<img src='/foo.png'>"
     index.exposed = True
 
 cherrypy.quickstart(Root(), '/', 'server.conf') 
